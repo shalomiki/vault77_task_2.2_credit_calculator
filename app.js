962ee97f31ea224d48311eeb823752ec04807f49
Timofey Biryukov <timofeybiryukov@gmail.com>
2019-02-25 19:02:45 +0600

    init commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// ваш код
+
+// подставте значениея
+document.getElementById('sum').innerHTML = '0';
+document.getElementById('time').innerHTML = '0';
+document.getElementById('interest').innerHTML = '0';
+document.getElementById('result').innerHTML = '0';
