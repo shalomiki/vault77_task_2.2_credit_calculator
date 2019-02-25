2f8efebc59b01ac159438e607a545f4c026fb3e1
Timofey Biryukov <timofeybiryukov@gmail.com>
2019-02-25 20:14:50 +0600

    overflow field

--- app.js
+++ app.js
@@ -5,4 +5,5 @@
 document.getElementById('sum').innerHTML = '0';
 document.getElementById('time').innerHTML = '0';
 document.getElementById('interest').innerHTML = '0';
-document.getElementById('result').innerHTML = '0';
+document.getElementById('daily').innerHTML = '0';
+document.getElementById('overflow').innerHTML = '0';
